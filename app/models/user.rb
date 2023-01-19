f16c9a3a93288324d1b31e8ec9357ff67fa08f5d
Gaelle Lopez <lope.gaelle@gmail.com>
2023-01-19 18:51:20 +0100

    resolve bug beaches

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :beachs
+  has_many :beaches
   has_many :bookings
 end
