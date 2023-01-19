88835819ee0035064d6f828ad78637a8bd0fa720
jessica coueron <jcoueron@yahoo.fr>
2023-01-19 18:57:11 +0100

    add action new create

--- app/controllers/beachs_controller.rb
+++ app/controllers/beachs_controller.rb
@@ -8,8 +8,24 @@ class BeachsController < ApplicationController
   def new
     @beach = Beach.new
   end
+
+  def edit
+  end
+
+  def create
+    @beach = Beach.new(beach_params)
+    @beach.user = current_user
+    @beach.save
+  end
+
   def show
     @beach = Beach.find(params[:id])
     @beach = Beach.new
+  end
+
+  private
+
+  def beach_params
+    params.require(:beach).permit(:name, :location, :price, :description)
   end
 end
