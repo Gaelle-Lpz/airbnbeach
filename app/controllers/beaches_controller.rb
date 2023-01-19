9d7551bfd157047297fba50b245782d4ee71488f
jessica coueron <jcoueron@yahoo.fr>
2023-01-19 19:27:07 +0100

    controller et view corrigés

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -4,6 +4,9 @@ class BeachesController < ApplicationController
     @beaches = Beach.all
   end
 
+  def edit
+  end
+
   def new
     @beach = Beach.new
   end
@@ -17,38 +20,10 @@ class BeachesController < ApplicationController
   def show
     @beach = Beach.find(params[:id])
   end
-end
 
+  private
 
-
-
-
-
-def index
-  @beachs = Beach.all
-end
-
-def new
-  @beach = Beach.new
-end
-
-def edit
-end
-
-def create
-  @beach = Beach.new(beach_params)
-  @beach.user = current_user
-  @beach.save
-end
-
-def show
-  @beach = Beach.find(params[:id])
-  @beach = Beach.new
-end
-
-private
-
-def beach_params
-  params.require(:beach).permit(:name, :location, :price, :description)
-end
+  def beach_params
+    params.require(:beach).permit(:name, :location, :price, :description)
+  end
 end
