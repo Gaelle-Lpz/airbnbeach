7b690f1efde8a0dc10d30c90ee7250c1cee27b0b
Shaherazade Bendjelloun <shaherazadeb@gmail.com>
2023-01-19 18:58:05 +0100

    fix new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,13 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show]
+  before_action :set_beach, only: [:new, :create]
+
   def index
     @bookings = Booking.all
+  end
+
+  def show
+    @review = Reviw.new(bookig: @booking)
   end
 
   def new
@@ -9,11 +16,25 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    @booking.save
-    redirect_to booking_path(@booking)
+    @booking.beach = @beach
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to bookings_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def set_beach
+    @beach = Beach.find(params[:beach_id])
+  end
+
+  def set_booking
+    @lbooking = Booking.find(params[:id])
+  end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
