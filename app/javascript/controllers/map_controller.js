b5f42f251b09227dddaf401e2625cddbabd357be
Shaherazade Bendjelloun <shaherazadeb@gmail.com>
2023-01-26 21:18:17 +0100

    Globe map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,8 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v10",
+      projection: 'globe'
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
